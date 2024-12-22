a19d597a68470d62de2747e8dbf1fca224b734fe
Olga <82639253+ohincu@users.noreply.github.com>
2024-12-22 12:53:47 +0100

    Create viz-template-professional.py in assets

---
+++ viz-template-professional.py
@@ -0,0 +1,51 @@
+viz_template_professional = {
+    'layout': {
+        'font': {
+            'family': 'Montserrat',
+            'size': 14,
+            'color': '#333333'
+        },
+        'plot_bgcolor': '#f8f8f8',  # Light grey background color
+        'paper_bgcolor': '#f8f8f8',  # Light grey background color
+        'xaxis': {
+            'showgrid': False,  # Disable x-axis gridlines
+            'title': {
+                'font': {
+                    'family': 'Montserrat',
+                    'size': 16,
+                    'color': '#333333'
+                }
+            }
+        },
+        'yaxis': {
+            'showgrid': False,  # Disable y-axis gridlines
+            'title': {
+                'font': {
+                    'family': 'Montserrat',
+                    'size': 16,
+                    'color': '#333333'
+                }
+            }
+        },
+        'title': {
+            'text': '',
+            'font': {
+                'family': 'Montserrat',
+                'size': 20,
+                'color': '#333333'
+            }
+        },
+        'colorway': [       '#4E79A7',  # blue
+                            '#F28E2B',  # orange
+                            '#E15759',  # red
+                            '#76B7B2',  # teal
+                            '#59A14F',  # green
+                            '#EDC948',  # yellow
+                            '#B07AA1',  # purple
+                            '#FF9DA7',  # pink
+                            '#9C755F',  # brown
+                            '#BAB0AC'   # gray
+                    ]
+    }
+}
+
